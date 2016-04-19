34af6972c342ae40261c719632e5bdbca0499fbc
Jeff Sternberg <jeffrey.sternberg@jsternberg-mac.local>
2016-04-19 11:50:37 -0400

    add ipreo settings

---
+++ readthedocs/settings/ipreo.py
@@ -0,0 +1,20 @@
+import os
+
+from .dev import CommunityDevSettings
+
+
+class IpreoSettings(CommunityDevSettings):
+
+    ALLOW_PRIVATE_REPOS = True
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': 'http://127.0.0.1:9200/',
+            'INDEX_NAME': 'haystack',
+        },
+    }
+
+IpreoSettings.load_settings(__name__)
