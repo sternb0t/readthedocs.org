b569d3a445e2bbf6c357f009b10e9958526e7b6c
Jeff Sternberg <jeffsternberg@gmail.com>
2016-04-21 16:08:34 -0400

    settings again

--- readthedocs/settings/ipreo.py
+++ readthedocs/settings/ipreo.py
@@ -6,19 +6,20 @@ from .dev import CommunityDevSettings
 class IpreoSettings(CommunityDevSettings):
 
     ALLOW_PRIVATE_REPOS = True
-    DEBUG = False
-    TEMPLATE_DEBUG = False
+    # DEBUG = False
+    # TEMPLATE_DEBUG = False
 
-    HAYSTACK_CONNECTIONS = {
-        'default': {
-            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-            'URL': 'http://127.0.0.1:9200/',
-            'INDEX_NAME': 'readthedocs',
-        },
-    }
+    # HAYSTACK_CONNECTIONS = {
+    #     'default': {
+    #         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+    #         'URL': 'http://127.0.0.1:9200/',
+    #         'INDEX_NAME': 'readthedocs',
+    #     },
+    # }
 
     SLUMBER_API_HOST = 'http://http://ipreo-readthedocs.eastus.cloudapp.azure.com'
     PRODUCTION_DOMAIN = 'ipreo-readthedocs.eastus.cloudapp.azure.com'
+    WEBSOCKET_HOST = 'ipreo-readthedocs.eastus.cloudapp.azure.com'
 
     # DOCKER_ENABLED = True
     # DOCKER_IMAGE = "rtfd-build:base"
